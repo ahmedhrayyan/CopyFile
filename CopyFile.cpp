5821f55069eb9a733cc8fa1521ef282dc6de3bf0
Ahmed Hamed <ahmedhrayyan@outlook.com>
2022-03-02 10:03:01 +0200

    Add project files.

---
+++ CopyFile.cpp
@@ -0,0 +1,55 @@
+//
+
+#include <iostream>
+#include <fstream>
+#include <Windows.h>
+
+std::string GetLastErrorAsString();
+
+int main(int argc, char* argv[])
+{
+	char* sourceFile = argv[1];
+	char* destinationFile = argv[2];
+	if (!CopyFileA(sourceFile, destinationFile, true))
+	{
+        std::cout << GetLastErrorAsString();
+	}
+	return 0;
+}
+
+// Returns the last Win32 error, in string format. Returns an empty string if there is no error.
+// credit: https://stackoverflow.com/a/17387176/10272966 
+std::string GetLastErrorAsString()
+{
+    //Get the error message ID, if any.
+    DWORD errorMessageID = ::GetLastError();
+    if(errorMessageID == 0) {
+        return std::string(); //No error message has been recorded
+    }
+    
+    LPSTR messageBuffer = nullptr;
+
+    //Ask Win32 to give us the string version of that message ID.
+    //The parameters we pass in, tell Win32 to create the buffer that holds the message for us (because we don't yet know how long the message string will be).
+    size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+                                 NULL, errorMessageID, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&messageBuffer, 0, NULL);
+    
+    //Copy the error message into a std::string.
+    std::string message(messageBuffer, size);
+    
+    //Free the Win32's string's buffer.
+    LocalFree(messageBuffer);
+            
+    return message;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
